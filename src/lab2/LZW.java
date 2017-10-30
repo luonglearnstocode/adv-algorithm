65d222db74e4d766f9c7bcbf2a15c3522545e5c9
Luong Nguyen <luong.nguyen@metropolia.fi>
2017-10-30 13:54:48 +0200

    change from writeInt to writeShort to decrease output file size

--- src/lab2/LZW.java
+++ src/lab2/LZW.java
@@ -19,6 +19,7 @@ import java.util.Map;
  */
 public class LZW {
     final static String INPUT = "input2c.txt";  
+//    final static String INPUT = "input2cOther.txt"; 
     final static String OUTPUT = "output2c.txt"; 
     final static String OUTPUT2 = "decompressed.txt";
     
@@ -43,7 +44,7 @@ public class LZW {
         System.out.println("###########################################################");
         System.out.println("Compressing");
         String content = readFile(INPUT);
-        System.out.println(content);
+        System.out.println("Original content: " + content);
         DataOutputStream output = new DataOutputStream(new FileOutputStream(OUTPUT));
         
         // initialize symbol table
@@ -53,24 +54,24 @@ public class LZW {
             map.put("" + (char) i, i);
 //            System.out.println("" + (char) i + " " + i);
         }    
+//        i++;
   
         while (content.length() > 0) {
             // get longest prefix
             System.out.println("content: " + content);
             String longestPrefix = findLongestPrefix(map, content);
-            if (longestPrefix == null) break;
-            
+            if (longestPrefix == null) break;            
             System.out.println("Longest prefix: " + longestPrefix + " code: " + map.get(longestPrefix));
-//            System.out.println(map.get(longestPrefix));
-            output.writeInt(map.get(longestPrefix));
+//            output.writeInt(map.get(longestPrefix)); //https://cs.wmich.edu/gupta/teaching/cs1120/1120Fall12web/codeJava/advanceFileIO.txt
+            output.writeShort(map.get(longestPrefix)); // https://www.tutorialspoint.com/java/java_dataoutputstream.htm
             // add to table
             int len = longestPrefix.length();
             if (len < content.length()) {
-                map.put(content.substring(0, len + 1), ++i);
+                map.put(content.substring(0, len + 1), i++);
                 System.out.println("\tPut " + content.substring(0, len + 1) + "->" + i);
             }               
                 
-            content = content.substring(len);
+            content = content.substring(len); // the remaining of content
 //            System.out.println(longestPrefix);
         }
         output.close();
@@ -87,22 +88,24 @@ public class LZW {
             map.put(i, "" + (char) i);
 //            System.out.println("" + (char) i + " " + i);
         }    
-        map.put(i++, "");
+//        map.put(i++, "");
         
         DataInputStream input = new DataInputStream(new BufferedInputStream(new FileInputStream(OUTPUT)));
         DataOutputStream output = new DataOutputStream(new FileOutputStream(OUTPUT2));
-        
-        Integer code = input.readInt();
+        Integer code = (int) input.readShort();
+//        Integer code = input.readInt();
         String p = map.get(code);
         
         try {
             while (true) {
-                System.out.println("current code: " + code + " - string: " + p);
+                System.out.println(code + " represents " + p);
                 output.write(p.getBytes());
-                code = input.readInt();
+//                code = input.readInt();
+                code = (int) input.readShort();
                 String s = map.get(code);
-                if (code == i) s = p + p.charAt(0); // code not in table
-                System.out.println("Next code " + code + " next string: " + s + " i: " + i);
+//                When a code is not in the table, its key is lastP followed by first character of lastP
+                if (code == i) s = p + p.charAt(0); 
+                System.out.println("Next code " + code + " next string: " + s);
                 
                 System.out.println("\tPutting " + i + "->" + (p + s.charAt(0)));
                 map.put(i++, p + s.charAt(0));
