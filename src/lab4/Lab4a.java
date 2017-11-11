ef8dd29fe1903cea72e99db563deca6cc98e130e
luonglearnstocode <luong.nguyen@metropolia.fi>
2017-11-11 22:13:05 +0200

    add lab4a method minmax recursive

--- src/lab4/Lab4a.java
+++ src/lab4/Lab4a.java
@@ -1,8 +1,13 @@
 package lab4;
 
+import java.io.PrintWriter;
 import java.security.SecureRandom;
 
 public class Lab4a {
+    private static final int STARTN = 100;
+    private static final int ENDN = 1000;
+    private static final int INCN = 100;
+    private static final String OUTPUTFILE = "lab4a.csv";
     static private Integer[] collection;
     static private SecureRandom rnd = new SecureRandom();
 
@@ -22,20 +27,62 @@ public class Lab4a {
         }
         System.out.println();
     }
-    private final static int N = 10;
+    private final static int N = 16;
     
     public static void main(String[] args) {
-        int max, min;
-        initTestSeq(N); printTestSeq();
-        MinMax minmax = new MinMax(collection);
-        minmax.minmax2();
-        System.out.println("Brute Force minmax search");
-        System.out.println("Min index " + minmax.getMin() + ", max index " + minmax.getMax());
-        System.out.println("Number of comparisions " + minmax.getComparisons());
-//        minmax = new MinMax(collection);
+//        int max, min;
+        
+//        initTestSeq(N); printTestSeq();
+//        
+//        MinMax minmax = new MinMax(collection);
+//        minmax.minmax2();
+//        System.out.println("N = " + N);
+//        System.out.println("Brute Force minmax search");
+//        System.out.println("Min index " + minmax.getMin() + ", max index " + minmax.getMax());
+//        System.out.println("Number of comparisions " + minmax.getComparisons());
+//        minmax = new MinMax(collection);        
 //        minmax.minmax();
 //        System.out.println("Divide and Conquer minmax search");
 //        System.out.println("Min index " + minmax.getMin() + ", max index " + minmax.getMax());
 //        System.out.println("Number of comparisions " + minmax.getComparisons());
+        
+        try {
+            PrintWriter writer = new PrintWriter(OUTPUTFILE, "UTF-8");
+
+            writer.println("N;conventional;minmax");
+
+            for (int n = STARTN; n <= ENDN; n += INCN) {
+                System.out.println("N: " + n);
+                initTestSeq(n);
+                
+                MinMax minmax = new MinMax(collection);
+                minmax.minmax2();
+                int comparisons = minmax.getComparisons();
+                System.out.println("N = " + N);
+                System.out.println("Brute Force minmax search");
+                System.out.println("Min index " + minmax.getMin() + ", max index " + minmax.getMax());
+                System.out.println("Number of comparisions " + comparisons);
+                
+                StringBuilder result = new StringBuilder(); 
+                result.append(n);                
+                result.append(";"); 
+                result.append(comparisons);
+                result.append(";");
+                
+                minmax = new MinMax(collection);        
+                minmax.minmax();
+                comparisons = minmax.getComparisons();
+                System.out.println("Divide and Conquer minmax search");
+                System.out.println("Min index " + minmax.getMin() + ", max index " + minmax.getMax());
+                System.out.println("Number of comparisions " + comparisons);
+                result.append(comparisons);
+                
+                writer.println(result.toString());
+            }
+            
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/lab4/MinMax.java
+++ src/lab4/MinMax.java
@@ -29,17 +29,63 @@ public class MinMax {
         return comparisions;
     }
     
-    public void minmax2() {
+    public void minmax2() { // 2n - 2
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i].compareTo(arr[min]) < 0)
+            if (less(i, min))
                 min = i;
-            if (arr[i].compareTo(arr[max]) > 0)
+            if (less(max, i))
                 max = i;
-            comparisions += 2;
         }
     }
     
-    public void minmax() {
+    public void minmax() { // 3n/2 - 2
+        int N = arr.length;
+        int[] result = minmaxHelper(0, N - 1);
+        min = result[0];
+        max = result[1]; 
+    }
+    
+    /*
+    * T(2)  = 1
+    * T(n)  = 2T(n/2) + 2 
+    *       = 4T(n/4) + 4 + 2
+    *       = 8T(n/8) + 8 + 4 + 2
+    *       = ...
+    *       = n/2 + (2^1 + 2^2 + ... + 2^(logn-1)) 
+    *       = n/2 + 2^logn - 2 = n/2 + n - 2 = 3n/2 - 2
+    */
+    public int[] minmaxHelper(int lo, int hi) {
+        if (hi == lo)
+            return new int[]{lo, lo};
         
+        if (hi - lo == 1) {
+            if (less(lo, hi)) 
+                return new int[]{lo, hi};
+            else 
+                return new int[]{hi, lo}; 
+        } else {
+            int mid = (hi + lo) / 2;
+            int[] left = minmaxHelper(lo, mid);
+            int[] right = minmaxHelper(mid + 1, hi);
+            return new int[]{min(left[0], right[0]), max(left[1], right[1])};
+        }
+    }
+    
+    // return left if arr[left] < arr[right] otherwise right
+    public int min(int left, int right) {
+        return less(left, right) ? left : right;
+    }
+    
+    // return left if arr[left] > arr[right] otherwise right
+    public int max(int left, int right) {
+        return less(left, right) ? right : left;
+    }
+    
+    // return if arr[i] < arr[j] 
+    public boolean less(int i, int j) {
+//        System.out.println(comparisions + " Comparing arr[" + i + "] = " + arr[i] 
+//                + " vs arr[" + j + "] = " + arr[j]);
+        comparisions++;
+        return arr[i].compareTo(arr[j]) < 0;
     }
 }
